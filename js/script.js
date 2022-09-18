c96893ab52d7ba7dd60e457f8a26f6ab91ebab43
hmathir <hmathir@gmail.com>
2022-09-18 17:51:00 +0600

    Buy Now button and Details Modal Added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let dataSet = [];
+let cartArray = [];
 const fetchPhones = async () => {
     const res = await fetch('data/data.json');
     const data = await res.json();
@@ -24,7 +25,7 @@ const displayPhones = (phones) => {
             <div class="card-actions justify-between ">
 
                 <label onclick="handleModal('${id}')" for="my-modal-3" class="modal-button btn btn-secondary btn-outline w-[45%]"><i class="fa-solid fa-circle-info mr-3"></i>Details</label>
-                <button class="btn btn-primary btn-outline w-[45%]"><iclass="fa-solid fa-bag-shopping mr-3"></i class=>Buy Now</button>
+                <button onclick="handleBuyNow('${id}')" class="btn btn-primary btn-outline w-[45%]"><iclass="fa-solid fa-bag-shopping mr-3"></i class=>Buy Now</button>
             </div>
         </div>
         `;
@@ -35,7 +36,7 @@ const displayPhones = (phones) => {
 
 const handleModal = (id) => {
     const phone = dataSet.find(item => item.id === id);
-    const {id:productID, img, price, name} = phone;
+    const {id:phoneID, img, price, name} = phone;
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
     <div>
@@ -43,6 +44,35 @@ const handleModal = (id) => {
     </div>
     <h1 class="text-2xl">Name: ${name}</h1>
     <p>Price: ${price}</p>
-    <p>Product ID: ${productID}</p>`;
+    <p>Product ID: ${phoneID}</p>`;
 }
 
+
+const handleBuyNow = (id) => {
+    const cartItemContainer = document.getElementById('cart-item');
+    const phone = dataSet.find(item=> item.id === id);
+    const { img, name } = phone;
+    if(cartArray.indexOf(phone) === -1){
+        cartArray.push(phone);
+
+        const newCartItem = document.createElement('div');
+        newCartItem.classList.add("flex", "justify-between", "items-center", "bg-slate-400","p-4","rounded-md","mt-4")
+    
+        newCartItem.innerHTML = 
+        `
+        <div>
+            <img src="${img}" class="w-10" alt="">
+        </div>
+        <p class="text-white">${name}</p>
+        <input class="border-2 border-red-100 w-10 text-center rounded-md" type="text" value="1"
+            readonly>
+        <i class="fa-solid fa-trash text-red-700"></i>
+        `;
+        cartItemContainer.appendChild(newCartItem);
+    } else{
+        alert('Item Already Added');
+        
+    }
+
+ 
+}
